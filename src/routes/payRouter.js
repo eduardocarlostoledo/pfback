3641b9b77fa6d647dd8ee7ebbfc8611263e3255c
eduardo carlos toledo <113210953+eduardocarlostoledo@users.noreply.github.com>
2023-02-27 20:16:20 -0300

    Update payRouter.js

--- src/routes/payRouter.js
+++ src/routes/payRouter.js
@@ -50,7 +50,7 @@ payRouter.post("/create_preference", (req, res) => {
                   <a href="https://main--tangerine-muffin-10d4f4.netlify.app/"><svg class='succes_svg' width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#232326" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg></a>
                     <h1 class="succes_h1">Payment Successful</h1>
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_xXsXXnglKn4YmVFVx39Pd-0LgWqhiUVk5g&usqp=CAU" alt="" class='succes_img'>
-                    <a href="https://main--tangerine-muffin-10d4f4.netlify.app/" class="succes_a">Keep Buying</a>
+                    <a href="https://main--tangerine-muffin-10d4f4.netlify.app/Products" class="succes_a">Keep Buying</a>
                     <p class="succes_p">COMPUTER STORE</p>
                     <ul class="succes_ul">          
                       <li class="succes_li">Payment ID: ${paymentId}</li>
@@ -75,7 +75,7 @@ payRouter.post("/create_preference", (req, res) => {
               <a href="https://main--tangerine-muffin-10d4f4.netlify.app/"><svg class='pending_svg' width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#232326" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg></a>
               <h1 class="pending_h1"> Pending Pay !</h1>
               <img class='pending_img'src="https://img.freepik.com/fotos-premium/simbolo-signo-exclamacion-azul-atencion-o-icono-signo-precaucion-fondo-problema-peligro-alerta-representacion-3d-senal-advertencia_256259-2831.jpg" alt="">
-              <a href="https://main--tangerine-muffin-10d4f4.netlify.app/" class="succes_a">Keep Buying</a>
+              <a href="https://main--tangerine-muffin-10d4f4.netlify.app/Products" class="succes_a">Keep Buying</a>
               <p class="pending_p">COMPUTER STORE</p>
             </div>
           </body>
@@ -96,7 +96,7 @@ payRouter.post("/create_preference", (req, res) => {
 			<a href="https://main--tangerine-muffin-10d4f4.netlify.app/"><svg class='failure_svg' width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#232326" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg></a>
 			<h1 class="failure_h1"> Failure Pay!</h1>
 			<img class="failure_img" src="https://static.vecteezy.com/system/resources/thumbnails/017/178/563/small/cross-check-icon-symbol-on-transparent-background-free-png.png" alt="">
-			<a href="https://main--tangerine-muffin-10d4f4.netlify.app/" class="succes_a">Keep Buying</a>
+			<a href="https://main--tangerine-muffin-10d4f4.netlify.app/Products" class="succes_a">Keep Buying</a>
 			<p class="failure_p">COMPUTER STORE</p>
 		</div>
 	  </body>
